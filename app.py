8d7946ad9e6d7cf1fc0d4d6e7340681a03cac3c8
umarpathanist <umarpathanist@gmail.com>
2026-02-17 17:42:12 +0530

    app

--- app.py
+++ app.py
@@ -168,6 +168,8 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
+    
     app.run(debug=True, use_reloader=False) # use_reloader=False prevents double scheduler start
